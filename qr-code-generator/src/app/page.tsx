c052263a83690775151910d7477c71b32ee80af1
CauaPortoBello <cauaportobello@gmail.com>
2026-06-08 20:05:28 -0300

    :package: Custom QR Code Generator

---
+++ qr-code-generator/src/app/page.tsx
@@ -0,0 +1,186 @@
+"use client";
+import {QRCodeCanvas} from 'qrcode.react';
+import { FaUpload } from "react-icons/fa";
+import { useRef, useState } from "react";
+
+export default function Home() {
+	const [linkValue, setlinkValue] = useState<string>('');
+    const [fgColor, setFgColor] = useState<string>('#000000');
+	const [bgColor, setBgColor] = useState<string>('#FFFFFF');
+	const [LogoUrl,setLogoUrl] = useState<string>('/logocauadev.png')
+	const [logoSize, setLogoSize] = useState<number>(22);
+	const qrCodeRef = useRef<HTMLDivElement>(null);
+
+	const handleLogoChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		const file = e.target.files?.[0];
+
+		if(file){
+			const reader = new FileReader();
+
+			reader.onload = () => {
+				if(reader.result) {
+					setLogoUrl (reader.result as string);
+				}
+			}
+
+			reader.readAsDataURL(file);
+		}
+	}
+
+	const handLeDownLoad = () => {
+
+		if (!qrCodeRef.current) return;
+
+		const canvas = qrCodeRef.current.querySelector("canvas");
+
+		if(!canvas) return;
+
+		const link = document.createElement("a");
+
+		link.href = canvas.toDataURL ("image/png");
+		link.download = "qrcode.png";
+		link.click();
+	}
+
+   return (
+     <main className="container">
+		<section className="title-container">
+			<h1 className="page-title">
+				Gere e customize 
+				QR Codes <span>dinâmicos</span>
+			</h1>
+
+		</section>	
+
+		<section className="qr-code-container">
+				<div className="qr-code">
+
+					 <div className="link-input">
+							<label>
+								Digite seu link
+							</label>
+							<input
+							type="text"
+							id="link"
+							placeholder="Seu link aqui"
+							value={linkValue}
+							onChange={(e) => setlinkValue(e.target.value)}
+							/>
+					 </div>
+					
+					 <div className="qr-code-preview">
+							<p>
+								QR Code Preview
+							</p>
+
+					<div ref={qrCodeRef}>
+
+						<QRCodeCanvas
+								value={linkValue}
+								title={linkValue}
+								size={200}
+								fgColor={fgColor}
+								bgColor={bgColor}
+								imageSettings={{
+									src: LogoUrl,
+									x: undefined,
+									y: undefined,
+									height: logoSize,
+									width: logoSize,
+									opacity: 1,
+									excavate: true,
+									crossOrigin: 'anonymous'
+								}}
+								/>
+					</div>
+
+					 </div>
+				</div>
+
+				<div className="qr-code-customization">
+
+					<div className='customization-container'>
+						<h3>
+							Cores 
+						</h3>
+
+						<div className='input-container colors'>
+							<div className='input-box'>
+								<label htmlFor="fgColor">
+									Cor Principal
+								</label>
+								<input
+									type='color'
+									className='input-color'
+									id='fgColor'
+									value={fgColor}
+									onChange={(e) => setFgColor(e.target.value)}
+									/>
+							</div>
+							<div className='input-box'>
+								<label htmlFor="bgColor">
+									Cor do fundo
+								</label>
+								<input
+									type='color'
+									className='input-color'
+									id='bgColor'
+									value={bgColor}
+									onChange={(e) => setBgColor(e.target.value)}
+									/>
+							</div>
+						</div>
+					</div>
+
+					<div className='customization-container'>
+                        <h3>
+							Logo
+						</h3>
+
+						<div className='input-container'>
+							<div className='input-box'>
+								<label htmlFor="logo">
+									Insira a sua logo
+								</label>
+								<input
+									type='file'
+									className='input-file'
+									id='Logo'
+									accept='image/*'
+									onChange={handleLogoChange}
+									/>
+
+									<button className='input-file-button'>
+										<FaUpload />
+									Escolher arquivo
+									</button>
+							</div>
+							<div className='input-box'>
+								<label htmlFor="logoSize">
+									Tamanho da logo
+								</label>
+								<select 
+								name="logoSize" 
+								id="logoSize"
+								value={logoSize}
+								onChange={(e) => setLogoSize(Number(e.target.value))}
+								>
+									<option value="24"> 14px x 14px</option>
+									<option value="32"> 22px x 22px</option>	
+								</select>
+							</div>
+						</div>
+						
+					</div>
+
+					<button className='dowload-button' onClick={handLeDownLoad}>
+						Baixar QR Code
+					</button>
+
+				</div>
+
+		</section>
+
+	 </main>
+   );
+}
